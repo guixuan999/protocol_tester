2dd286c3e36a61d8214add9026412c3cfec86137
guixuan <a20137@163.com>
2024-08-28 10:59:55 +0800

    add element-plus

--- electron/main.cjs
+++ electron/main.cjs
@@ -4,14 +4,16 @@ const NODE_ENV = process.env.NODE_ENV;
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.cjs'),
       nodeIntegration: true, // 如果你需要在渲染进程中使用 Node.js API
       contextIsolation: true, // 允许直接访问 Electron 和 Node.js API
     },
   });
+
+  mainWindow.maximize();
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     mainWindow.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,101 @@
 <script setup>
-import { onMounted } from 'vue'
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+	import {
+		onMounted, 
+		ref
+	} from 'vue'
+	import {
+		RouterLink,
+		RouterView
+	} from 'vue-router'
+	import HelloWorld from './components/HelloWorld.vue'
 
-import router from "@/router"
-// THIS ALSO WORKS
-// import router from "./router/index.js"
+	import router from "@/router"
+	// THIS ALSO WORKS
+	// import router from "./router/index.js"
 
-onMounted(() => {
-  router.push('/')
-})
+	onMounted(() => {
+		router.push('/')
+	})
 
+	const micmote_logo = ref(new URL('@/assets/logo.png', import.meta.url).href)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+	<el-header>
+		<el-row justify="space-between" style="padding: 1.25em 0">
+			<el-col :span="12">
+				<el-image :src="micmote_logo" style="height: 1.125em" />
+			</el-col>
+			<el-col :span="12">
+				<RouterLink to="/" class="custom-link">首页</RouterLink>
+				<RouterLink to="/about" class="custom-link">关于</RouterLink>
+			</el-col>
+		</el-row>
+	</el-header>
+	<el-main>
+		<RouterView />
+	</el-main>
+	<el-footer>Copyright micmote 2024</el-footer>
 
-    <div class="wrapper">
-      <HelloWorld msg="who did this?" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+	.el-main {
+	  padding: 0
+	}
+	
+	.custom-link.router-link-active {
+	  color: blue;
+	}
+	
+	.custom-link {
+	  padding: 0px 0.3125em;
+	  /* 上下0px，左右20px */
+	  text-decoration: none;
+	  color: black;
+	}
+	
+	/* The following might not be necessory, cause user agent style sheet for <a> has cursor: pointer  */
+	.custom-link:hover {
+	  cursor: pointer
+	}
+	
+	.el-header {
+	  background-color: #EBEEF5F0;
+	  height: 3.5em;
+	  width: 100%;
+	  /*
+		position: fixed;
+	  top: 0;
+	  left: 0;
+	  z-index: 100;
+		*/
+	}
+	
+	.el-footer {
+	  height: 100%;
+	  width: 100%;
+	  background-color: lightgray;
+	  color: white;
+	  padding: 1em 0;
+		display: flex;
+		justify-content: center;
+	}
+	
+	.demonstration {
+	  color: var(--el-text-color-secondary);
+	}
+	
+	h1 {
+	  font-size: 1em;
+	}
+	
+	p {
+	  font-size: 0.9em;
+	  color: #BBBBBB
+	}
+	
+	.el-divider {
+	  margin: 0.623em 0
+	}
 </style>
