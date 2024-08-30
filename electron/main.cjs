6c53bb4665f9376a0c3da50bf35ad7397da1bdb1
guixuan <a20137@163.com>
2024-08-30 15:18:44 +0800

    move database interfaces into independent file

--- electron/main.cjs
+++ electron/main.cjs
@@ -3,53 +3,7 @@ const path = require('path');
 const { SerialPort } = require('serialport')
 const { fork } = require('node:child_process');
 const get_now_str = require('./utils.cjs')
-
-const sqlite3 = require('sqlite3').verbose();
-
-
-// 创建表（如果不存在）
-function createTableIfNotExists(tableName, columns) {
-  const columnsDefinition = Object.entries(columns)
-    .map(([name, type]) => `${name} ${type}`)
-    .join(', ');
-  const sql = `CREATE TABLE IF NOT EXISTS ${tableName} (${columnsDefinition})`;
-
-  database.run(sql, (err) => {
-    if (err) {
-      console.error(`Failed to create table ${tableName}:`, err.message);
-    } else {
-      console.log(`Table ${tableName} is ready.`);
-    }
-  });
-}
-
-// 插入一条记录到指定表中
-function insertRecord(table, data) {
-  const placeholders = Object.keys(data).map(() => '?').join(',');
-  const sql = `INSERT INTO ${table} (${Object.keys(data).join(',')}) VALUES (${placeholders})`;
-
-  database.run(sql, Object.values(data), function(err) {
-    if (err) {
-      console.error('Failed to insert record:', err.message);
-    } else {
-      console.log(`Record inserted with rowid ${this.lastID}`);
-    }
-  });
-}
-
-// 打开已有的数据库文件
-let database = new sqlite3.Database('./database.db', (err) => {
-  if (err) {
-    console.error('Failed to open database:', err.message);
-    process.exit(1);
-  }
-  console.log('Connected to the SQLite database.');
-  createTableIfNotExists('packages', {date: "text", direction: "text", raw: "text"})
-  
-});
-
-
-
+const { initDatabase, insertRecord } = require("./database.cjs")
 
 const NODE_ENV = process.env.NODE_ENV;
 
@@ -114,6 +68,8 @@ function createWindow() {
     }
   }
 }
+
+initDatabase()
 
 app.whenReady().then(() => {
   ipcMain.handle('list_serial', handleListSerial)
